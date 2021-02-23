79b4a88e358d108dbe724c1627514e80c5b5ac4e
Furkan <furkvnclk@gmail.com>
2021-02-23 14:14:30 +0300

    Initial Commit

---
+++ app/Imports/UsersImport.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class UsersImport implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new User([
+            'name' => $row[0],
+            'email' => $row[0] . '@email.com',
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
+}
